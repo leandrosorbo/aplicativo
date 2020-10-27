8f34e827750147417fafff51f6bcbaaee7714056
naelfox <natanaelvila2@gmail.com>
2020-10-27 01:18:39 -0300

    aplicacao do placeholder

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,23 @@
 import { Component } from '@angular/core';
 import { ActionSheetController, Platform, AlertController } from '@ionic/angular';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  [x: string]: any;
 
-  selectedDateString1: string = new Date().toISOString();
+  selectedDateString1 = new Date().toISOString();
   minDateInput: string = new Date().toISOString();
   maxDateInput: string = new Date().toISOString();
 
-  selectedDateString2: string = new Date().toISOString();
+  selectedDateString2 = new Date().toISOString();
   minDateOutput: string = new Date().toISOString();
   maxDateOutput: string = new Date().toISOString();
 
-
- constructor(public actionSheetController: ActionSheetController, private platform1: Platform, private platform2: Platform) {
+ constructor(private platform1: Platform, private platform2: Platform, private alertCrtl: AlertController) {
 this.platform1.ready().then(() => {
 let date1: Date = new Date();
 date1.setDate(date1.getDate() - 0);
@@ -33,8 +31,8 @@ this.maxDateInput = date1.toISOString();
 });
 this.platform2.ready().then(() => {
   let date2: Date = new Date();
-  date2.setDate(date2.getDate() + 1);
-  this.minDateOutput = date2.toISOString();
+  date2.setDate(date2.getDate() - 0);
+  this.minDateInput = date2.toISOString();
 
   date2 = new Date();
   date2.setDate(date2.getDate() + 365);
@@ -43,10 +41,15 @@ this.platform2.ready().then(() => {
 
 );
 
-
-
-
-
-
 }
+
+
+
+
+updateLocalStorage(){
+    localStorage.setItem('chave', JSON.stringify(this.selectedDateString1));
+  }
+
+
+
 }
