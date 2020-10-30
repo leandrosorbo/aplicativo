1e1fdaf534202c982b67b156567ff37df122c4c5
Leandro <leandro.sorbo@gmail.com>
2020-10-30 11:35:46 -0300

    Implementação do Storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Platform, AlertController } from '@ionic/angular';
+import { ActionSheetController, Platform, AlertController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
+
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,9 @@ import { Platform, AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  [x: string]: any;
+
+  inputtext: string;
+  key = 'username';
 
   selectedDateString1 = new Date().toISOString();
   minDateInput: string = new Date().toISOString();
@@ -17,39 +21,43 @@ export class HomePage {
   minDateOutput: string = new Date().toISOString();
   maxDateOutput: string = new Date().toISOString();
 
- constructor(private platform1: Platform, private platform2: Platform, private alertCrtl: AlertController) {
-this.platform1.ready().then(() => {
-let date1: Date = new Date();
-date1.setDate(date1.getDate() - 0);
-this.minDateInput = date1.toISOString();
+  constructor(private platform1: Platform, private platform2: Platform, private storage: Storage) {
 
-date1 = new Date();
-date1.setDate(date1.getDate() + 365);
-this.maxDateInput = date1.toISOString();
+    this.platform1.ready().then(() => {
+      let date1: Date = new Date();
+      date1.setDate(date1.getDate() - 0);
+      this.minDateInput = date1.toISOString();
 
-
-});
-this.platform2.ready().then(() => {
-  let date2: Date = new Date();
-  date2.setDate(date2.getDate() - 0);
-  this.minDateInput = date2.toISOString();
-
-  date2 = new Date();
-  date2.setDate(date2.getDate() + 365);
-  this.maxDateOutput = date2.toISOString();
-}
-
-);
-
-}
+      date1 = new Date();
+      date1.setDate(date1.getDate() + 365);
+      this.maxDateInput = date1.toISOString();
 
 
+    });
+    this.platform2.ready().then(() => {
+      let date2: Date = new Date();
+      date2.setDate(date2.getDate() - 0);
+      this.minDateInput = date2.toISOString();
 
+      date2 = new Date();
+      date2.setDate(date2.getDate() + 365);
+      this.maxDateOutput = date2.toISOString();
 
-updateLocalStorage(){
-    localStorage.setItem('chave', JSON.stringify(this.selectedDateString1));
+    }
+
+    );
+
   }
 
+  saveData() {
+    this.storage.set(this.key, this.inputtext);
+  }
+
+  loadData(){
+  this.storage.get(this.key).then((val) => {
+    console.log('Your username is', val);
+  });
+}
 
 
 }
