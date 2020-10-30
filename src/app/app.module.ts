45b2eee2fcdedeaf7d6f72b161cc4fda8b6b32d5
Leandro <leandro.sorbo@gmail.com>
2020-10-30 11:18:09 -0300

    Instalado  o storage e subindo no app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { IonicStorageModule } from '@ionic/storage'
+
 //firebase
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
@@ -17,7 +19,7 @@ import { config } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(), 
     //firebase imports
     AngularFireModule.initializeApp(config), AngularFireAuthModule],
   providers: [
